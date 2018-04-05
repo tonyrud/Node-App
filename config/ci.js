66f7ebef6aae6b8a76b044cfca4e75c8c56df73a
Tony Rudny <tonyrudny@tony-rudny.sweetwater.com>
2018-04-04 21:32:39 -0400

    fix ci config

--- config/ci.js
+++ config/ci.js
@@ -3,6 +3,6 @@ module.exports = {
         '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
     googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
     mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
-    redisUrl = 'redis://127.0.0.1:6379',
+    redisUrl: 'redis://127.0.0.1:6379',
     cookieKey: '123123123',
 };
