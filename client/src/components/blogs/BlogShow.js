13e5b3592a06884039af4fbfbcbecfffd3de6136
Tony Rudny <tonyrudny@tony-rudny.sweetwater.com>
2018-04-14 08:12:41 -0400

    s3 image bucket upload setup

--- client/src/components/blogs/BlogShow.js
+++ client/src/components/blogs/BlogShow.js
@@ -3,28 +3,42 @@ import { connect } from 'react-redux';
 import { fetchBlog } from '../../actions';
 
 class BlogShow extends Component {
-  componentDidMount() {
-    this.props.fetchBlog(this.props.match.params._id);
-  }
-
-  render() {
-    if (!this.props.blog) {
-      return '';
+    componentDidMount() {
+        this.props.fetchBlog(this.props.match.params._id);
     }
 
-    const { title, content } = this.props.blog;
+    renderImage() {
+        if (this.props.blog.imageUrl) {
+            return (
+                <img
+                    src={`https://s3.us-east-2.amazonaws.com/blog-bucket-1234/${
+                        this.props.blog.imageUrl
+                    }`}
+                    alt="bucket"
+                />
+            );
+        }
+    }
 
-    return (
-      <div>
-        <h3>{title}</h3>
-        <p>{content}</p>
-      </div>
-    );
-  }
+    render() {
+        if (!this.props.blog) {
+            return '';
+        }
+
+        const { title, content } = this.props.blog;
+
+        return (
+            <div>
+                <h3>{title}</h3>
+                <p>{content}</p>
+                {this.renderImage()}
+            </div>
+        );
+    }
 }
 
 function mapStateToProps({ blogs }, ownProps) {
-  return { blog: blogs[ownProps.match.params._id] };
+    return { blog: blogs[ownProps.match.params._id] };
 }
 
 export default connect(mapStateToProps, { fetchBlog })(BlogShow);
--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,11 +1,27 @@
 const AWS = require('aws-sdk');
+const uuid = require('uuid/v1');
 const keys = require('./../config/keys');
+const requireLogin = require('./../middlewares/requireLogin');
 
 const s3 = new AWS.S3({
     accessKeyId: keys.accessKeyId,
     secretAccessKey: keys.secretAccessKey,
+    signatureVersion: 'v4',
+    region: 'us-east-2',
 });
 
 module.exports = app => {
-    app.get('/api/upload', (req, res) => {});
+    app.get('/api/upload', requireLogin, (req, res) => {
+        const key = `${req.user.id}/${uuid()}.jpeg`;
+        s3.getSignedUrl(
+            'putObject',
+            {
+                Bucket: 'blog-bucket-1234',
+                ContentType: 'image/jpeg',
+                Key: key,
+            },
+            // send key for db save, don't use full url...might want to change the bucket location
+            (err, url) => res.send({ key, url }),
+        );
+    });
 };
